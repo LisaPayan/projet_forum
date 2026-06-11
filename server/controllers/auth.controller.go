7072ff6ca499335172a9f8924fc7fdd0fe39bd8c
Cromca <fuentes.quentin.13@gmail.com>
2026-06-11 09:24:41 +0200

    auth.controller et creation auth.router

--- server/controllers/auth.controller.go
+++ server/controllers/auth.controller.go
@@ -8,3 +8,27 @@ import (
 	"projet_forum/helpers"
 )
 
+type AuthController struct {
+	service *services.AuthService
+}
+
+func initAuthController(authService *services.AuthService) *AuthController {
+	return &AuthController{service: authService}
+}
+
+func (c *AuthController) Register(w http.ResponseWriter, r *http.Request) {
+	var data dto.RegisterRequestdto
+
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		helpers.writeerrorResponse(w, http.StatusBadRequest, "JSON invalide")
+		return
+	}
+
+	response, err := c.service.Register(data)
+	if err != nil {
+		helpers.writeerror(w, http.StatusBadRequest, err.Error()
+		return
+	}
+
+	helpers.writeJSONResponse(w, http.StatusCreated, response)
+}
